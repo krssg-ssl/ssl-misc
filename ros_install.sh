e61f77635eb97706d59931edf41710ca90e79c9a
Animesh <annimesh2809@gmail.com>
2017-04-30 17:03:36 +0530

    Testing #2

--- ros_install.sh
+++ ros_install.sh
@@ -6,11 +6,11 @@
 		##################################################################
 
 ####################### For proxy 
-KGP_PROXY="10.3.100.207"
-KGP_PORT="8080"
+#KGP_PROXY="10.3.100.207"
+#KGP_PORT="8080"
 
-export http_proxy="http://$KGP_PROXY:$KGP_PORT"
-export https_proxy="https://$KGP_PROXY:$KGP_PORT"
+#export http_proxy="http://$KGP_PROXY:$KGP_PORT"
+#export https_proxy="https://$KGP_PROXY:$KGP_PORT"
 #######################
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
