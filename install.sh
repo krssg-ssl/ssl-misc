8dee4bdbd376709734f4e422243d45781599ce0a
Animesh Kashyap <annimesh2809@gmail.com>
2017-04-30 23:21:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,20 +11,20 @@ workspace_dir=$1
 
 echo "Installing Dependencies"
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-# sudo apt-get -y update
-# sudo apt-get install -y g++-4.9
-# echo "Installing latest qt5"
-# sudo apt-get install -y qt5-default
-# sudo apt-get install -y cmake libqt5svg5-dev libprotobuf-dev protobuf-compiler libode-dev screen
+sudo apt-get -y update
+sudo apt-get install -y g++-4.9
+echo "Installing latest qt5"
+sudo apt-get install -y qt5-default
+sudo apt-get install -y cmake libqt5svg5-dev libprotobuf-dev protobuf-compiler libode-dev screen
 
 mkdir temp_dir && cd temp_dir
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/vartypes/vartypes-0.7.tar.gz
 tar xfz vartypes-0.7.tar.gz
 cd vartypes-0.7
 mkdir build && cd build
-# cmake ..
-# make -j8
-# sudo make install
+cmake ..
+make -j8
+sudo make install
 cd ../../../
 rm -rf temp_dir
 
@@ -34,9 +34,9 @@ mkdir temp_dir && cd temp_dir
 wget https://cmake.org/files/v3.8/cmake-3.8.0.tar.gz
 tar xf cmake-3.8.0.tar.gz
 cd cmake-3.8.0
-# ./configure
-# make -j8
-# sudo make install
+./configure
+make -j8
+sudo make install
 cd ../../
 rm -rf temp_dir
 
@@ -74,7 +74,7 @@ git clone https://github.com/krssg-ssl/skills.git
 git clone https://github.com/krssg-ssl/vision_comm.git
 git clone https://github.com/krssg-ssl/traj_controller.git
 git clone https://github.com/krssg-ssl/navigation.git
-git clone https://github.com/krssg-ssl/refBox.git
+# git clone https://github.com/krssg-ssl/refBox.git
 git clone https://github.com/krssg-ssl/krssg_ssl_msgs.git
 
 cd ..
